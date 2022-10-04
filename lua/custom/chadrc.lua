ed5e5a3468a31bd90cba0b77a23adc15dff469d6
Jake Walklate <jakewalklate@gmail.com>
2022-10-04 14:56:10 +1100

    update

---
+++ lua/custom/chadrc.lua
@@ -0,0 +1,62 @@
+-- First read our docs (completely) then check the example_config repo
+local M = {}
+
+M.ui = {
+  theme = "onedark",
+  transparency = true,
+  theme_toggle = { "gruvbox", "onedark" }
+}
+
+M.plugins = {
+  user = {
+    ["kyazdani42/nvim-tree.lua"] = {
+      event = "VimEnter"
+    },
+  },
+  override = {
+    ["NvChad/ui"] = {
+     tabufline = {
+        enabled = true,
+        lazyload = false,
+      },
+    },
+    ["kyazdani42/nvim-tree.lua"] = {
+      hijack_cursor = false,
+      open_on_setup = true,
+      open_on_setup_file = true,
+      git = {
+        enable = true,
+        show_on_dirs = true,
+      },
+      view = {
+        adaptive_size = false,
+      },
+      renderer = {
+        highlight_git = true,
+        icons = {
+          show = {
+            git = true
+          }
+        }
+      },
+      actions = {
+        open_file = {
+          resize_window = false,
+        }
+      }
+    },
+    ["NvChad/nvterm"] = {
+      terminals = {
+        shell = '/bin/zsh --login',
+        type_opts = {
+          horizontal = {
+            location = "rightbelow",
+            split_ratio = 0.275
+          },
+        },
+      }
+    }
+  }
+}
+
+return M
