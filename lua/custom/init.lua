f0ade7e93e9b9d69aef982b08f8a2197bbff78e6
Jake Walklate <jakewalklate@gmail.com>
2022-10-04 18:52:54 +1100

    update

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,1 +1,16 @@
-vim.cmd("echo Hello World")
+local function reSizeWindows()
+  for _, window in ipairs(vim.api.nvim_list_wins()) do
+    local buf = vim.api.nvim_win_get_buf(window)
+    local name = vim.api.nvim_buf_get_name(buf)
+    if string.match(name, 'NvimTree_1') then
+      vim.api.nvim_win_set_width(window, 25) -- reset nvim-tree size on exit buffer
+    end
+  end
+end
+
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  callback = function()
+    reSizeWindows()
+  end
+})
+
